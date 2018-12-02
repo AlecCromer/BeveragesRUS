0f502012c4eb15ebf5c0c189e9ab91e4200c4dfb
AlecCromer <33764772+AlecCromer@users.noreply.github.com>
2018-12-02 16:43:49 -0500

    Update webstore.sql

--- webstore.sql
+++ webstore.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 29, 2018 at 07:45 PM
--- Server version: 5.6.26
--- PHP Version: 5.6.12
+-- Generation Time: Dec 02, 2018 at 10:42 PM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,32 +19,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `webstore`
 --
+CREATE DATABASE  IF NOT EXISTS webstore;
 
+use webstore;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `customer`
 --
 
-CREATE DATABASE  IF NOT EXISTS webstore;
-
-use webstore;
-
-CREATE TABLE  IF NOT EXISTS `customer` (
+CREATE TABLE `customer` (
   `customerID` int(8) NOT NULL,
   `customerEmail` varchar(50) NOT NULL,
   `customerPassword` varchar(100) NOT NULL,
-  `customerName` varchar(100) NOT NULL
+  `customerName` varchar(100) NOT NULL,
+  `admin` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`customerID`, `customerEmail`, `customerPassword`, `customerName`) VALUES
-(2866, 'johncena@gmail.com', 'password', 'John'),
-(671341, 'cromeralec@gmail.com', 'password', 'Alec'),
-(681875, 'a', 'a', 'Pablo');
+INSERT INTO `customer` (`customerID`, `customerEmail`, `customerPassword`, `customerName`, `admin`) VALUES
+(681875, 'a@a.com', 'a', 'Pablo', 'a'),
+(736010, 'johnwick@gmail.com', 'password', 'John', 'na');
 
 -- --------------------------------------------------------
 
@@ -52,7 +50,7 @@ INSERT INTO `customer` (`customerID`, `customerEmail`, `customerPassword`, `cust
 -- Table structure for table `items`
 --
 
-CREATE TABLE IF NOT EXISTS `items` (
+CREATE TABLE `items` (
   `itemId` int(10) NOT NULL,
   `itemName` varchar(128) NOT NULL,
   `itemPrice` float NOT NULL,
@@ -65,7 +63,7 @@ CREATE TABLE IF NOT EXISTS `items` (
 --
 
 INSERT INTO `items` (`itemId`, `itemName`, `itemPrice`, `itemDescription`, `amount`) VALUES
-(1267, 'coca-coler', 1.29, 'Enjoy this refreshing beverage', 994),
+(1267, 'coca-coler', 1.29, 'Enjoy this refreshing beverage', 3338),
 (1368, 'Deadshot Daiquiri', 3.99, 'Tasty, probably. Refrigerate please', 998),
 (2347, 'Secret Sauce', 0.59, 'It''s a sauce made with secrets', 997),
 (3345, 'Speed Cola', 1.99, 'Speedy drink', 997),
@@ -82,7 +80,7 @@ INSERT INTO `items` (`itemId`, `itemName`, `itemPrice`, `itemDescription`, `amou
 -- Table structure for table `shoppingcart`
 --
 
-CREATE TABLE IF NOT EXISTS `shoppingcart` (
+CREATE TABLE `shoppingcart` (
   `orderId` int(6) NOT NULL,
   `userId` int(6) NOT NULL,
   `itemId` int(6) NOT NULL,
