cfcbb2180fdf0240c56d5bffa103729cea54af2b
AlecCromer <33764772+AlecCromer@users.noreply.github.com>
2019-03-09 12:27:39 -0800

    removed old files

--- BeveragesRus/js/script.js
+++
@@ -1,8 +0,0 @@
-function attachEvent(obj,evt,fnc,useCapture){
-  if (obj.addEventListener){
-    obj.addEventListener(evt,fnc,!!useCapture);
-    return true;
-  } else if (obj.attachEvent){
-    return obj.attachEvent(\"on\"+evt,fnc);
-  }
-} 
